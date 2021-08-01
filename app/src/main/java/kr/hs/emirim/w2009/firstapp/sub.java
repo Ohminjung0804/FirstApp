09152606b10dc0305205887bb6d9825577da540b
OHMINJUNG <w2009@e-mirim.hs.kr>
2021-08-01 12:26:13 +0900

    Completion Intent

--- app/src/main/java/kr/hs/emirim/w2009/firstapp/sub.java
+++ app/src/main/java/kr/hs/emirim/w2009/firstapp/sub.java
@@ -2,13 +2,24 @@ package kr.hs.emirim.w2009.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class sub extends AppCompatActivity {
 
+    TextView tv_sub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        tv_sub = findViewById(R.id.tv_sub);
+
+        Intent intent = getIntent(); //어디선가 오는 인텐트를 받는다
+        String str = intent.getStringExtra("str"); //main에서 오는 str을 str로 받음
+        tv_sub.setText(str);
     }
 }
